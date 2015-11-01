f74873e3129a324bc150f01878f24b8e1d304150
王明申 <1043978937@qq.com>
2015-11-01 11:52:22 +0800

    项目初始化

---
+++ collectionView布局/collectionView布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionView布局
+//
+//  Created by 王明申 on 15/11/1.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionView布局/collectionView布局/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  collectionView布局
+//
+//  Created by 王明申 on 15/11/1.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
