6e9c0a18c729558518d378726c63ae9953460ec2
王明申 <1043978937@qq.com>
2015-11-02 21:30:15 +0800

    优化

--- collectionView布局/collectionView布局/MyLayout.m
+++ collectionView布局/collectionView布局/MyLayout.m
@@ -13,10 +13,11 @@
     [super prepareLayout];
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
     self.itemSize = CGSizeMake(100, 100);
-    self.sectionInset = UIEdgeInsetsMake(0, 20, 0, 20);
+    self.sectionInset = UIEdgeInsetsMake(0, 25, 0, 25);
 }
-//返回collectionview上所有元素的布局属性
+//返回collectionview上所有元素的布局属性，显示在视图上
 -(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+//    NSLog(@"sss");
 //调用父类布局属性
     NSArray *array = [super layoutAttributesForElementsInRect:rect];
     //     实现缩放功能，获取collectionview中点，根据每个cell距离中点的距离进行缩放
@@ -37,5 +38,21 @@
 }
 -(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
     return  YES;
+}
+-(CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity {
+    CGRect rect;
+    rect.origin = proposedContentOffset;
+    rect.size = self.collectionView.frame.size;
+    NSArray *arrar = [self layoutAttributesForElementsInRect:rect];
+//    获取cell的布局属性
+    CGFloat centerX = proposedContentOffset.x + self.collectionView.frame.size.width * 0.5;
+    CGFloat minDelta = MAXFLOAT;
+    for (UICollectionViewLayoutAttributes *attrs in arrar) {
+        CGFloat cat = attrs.center.x - centerX;
+        if (ABS(cat) <= ABS(minDelta)) {
+            minDelta = cat;
+        }
+    }
+    return CGPointMake(proposedContentOffset.x + minDelta, proposedContentOffset.y);
 }
 @end
